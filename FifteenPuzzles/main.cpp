c2b5b8bf05e2f1ca0c23182adacd4deafc298fb0
Lukasz Firaza <lukasz.firaza@gmail.com>
2017-03-15 13:22:24 +0100

    Refactoring

--- FifteenPuzzles/main.cpp
+++ FifteenPuzzles/main.cpp
@@ -11,41 +11,61 @@
  * Created on 14 marca 2017, 13:19
  */
 
-#define N_ITEMS( array )   (sizeof( array )/sizeof( array[0] ))
-
 #include <cstdlib>
 #include <cmath>
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-bool ifSolvable(int []);
+bool ifSolvable(int [], int, int);
 /*
  * 
  */
 int main(int argc, char** argv) {
 
-    int test[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 14, 0};
+    int iloscWierszy = 4;
+    int iloscKolumn = 4;
     
-    cout<<ifSolvable(test);
+    int startUklad[iloscWierszy][iloscKolumn] = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+        {13,15,14,0}
+    };
+    
+    int startPlasko[iloscWierszy*iloscKolumn] = {0};
+    
+      for (int i = 0; i < iloscWierszy; i++){
+    
+        for (int j=0; j < iloscKolumn; j++){
+            
+            startPlasko[i*4+j] = startUklad[i][j];
+        }
+    
+    }
+    
+    
+    cout<<ifSolvable(startPlasko, iloscWierszy, iloscKolumn);
     
     return 0;
 }
 
-bool ifSolvable(int puzzle[]){
+bool ifSolvable(int puzzle[], int row, int col){
     int parity = 0;
-    int const length = 16;
-    int gridWidth = (int) sqrt(length);
-    int row = 0; 
+    int gridWidth = col;
+    int helperRow = 0;
     int blankRow = 0; 
+    int length = row * col;
+    
+  
 
     for (int i = 0; i < length; i++)
     {
         if (i % gridWidth == 0) { 
-            row++;
+            helperRow++;
         }
         if (puzzle[i] == 0) { 
-            blankRow = row; 
+            blankRow = helperRow; 
             continue;
         }
         for (int j = i + 1; j < length; j++)
